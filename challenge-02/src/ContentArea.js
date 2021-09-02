72017c5d40ca3ca0053aa3b597138c80d20ffb29
João Gabriel <main.joaogabrielsg@gmail.com>
2021-09-02 11:28:23 -0300

    :heavy_check_mark: [CHALLENGE 02] challenge 02 completed

--- challenge-02/src/ContentArea.js
+++ challenge-02/src/ContentArea.js
@@ -2,24 +2,11 @@ import { useState } from 'react';
 
 import H1 from './H1';
 
-export default function ContentArea({ post }) {
-  const [title, setTitle] = useState(post.title);
-  const [content, setContent] = useState(post.content);
-
-  if (post.title !== title || post.content !== content) {
-    setTitle(post.title)
-    setContent(post.content)
-  }
-
-  // useEffect(() => {
-  //   setTitle(post.title);
-  //   setContent(post.content);
-  // }, [post]);
-
+export default function ContentArea({ title, content }) {
   return (
     <main className="content-area">
       <H1>{title}</H1>
       {content}
     </main>
-  )
+  );
 }
--- challenge-02/src/app.js
+++ challenge-02/src/app.js
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import Header from "./Header";
-import Menu from "./Menu";
-import Sidebar from "./Sidebar";
-import ContentArea from "./ContentArea";
-import Footer from "./Footer";
+import Header from './Header';
+import Menu from './Menu';
+import Sidebar from './Sidebar';
+import ContentArea from './ContentArea';
+import Footer from './Footer';
 
-import H2 from "./H2";
+import H2 from './H2';
 
 const links = [
   {
@@ -15,7 +15,9 @@ const links = [
     content: (
       <>
         <p>
-          Existe dupla mais popular que essa? Arroz com feijão já se completam, combinados com carne-seca, queijo de coalho e bacon então... Vai  dar samba, ou melhor, baião!
+          Existe dupla mais popular que essa? Arroz com feijão já se completam,
+          combinados com carne-seca, queijo de coalho e bacon então... Vai dar
+          samba, ou melhor, baião!
         </p>
         <H2>Ingredientes</H2>
         <ul>
@@ -30,28 +32,55 @@ const links = [
         <H2>Modo de Preparo</H2>
         <ol>
           <li>
-            Corte a carne-seca em cubos grandes de cerca de 7 cm - este tamanho é ideal para dessalgar a carne sem tirar completamente o sal e  também para desfiar de um tamanho uniforme. Coloque numa tigela e lave em água corrente.
+            Corte a carne-seca em cubos grandes de cerca de 7 cm - este tamanho
+            é ideal para dessalgar a carne sem tirar completamente o sal e
+            também para desfiar de um tamanho uniforme. Coloque numa tigela e
+            lave em água corrente.
           </li>
           <li>
-            Cubra os cubos de carne com 5 xícaras (chá) de água fria. Cubra a tigela com um prato (ou filme) e deixe na geladeira por 24 horas - troque a água pelo menos uma vez durante este período. 
+            Cubra os cubos de carne com 5 xícaras (chá) de água fria. Cubra a
+            tigela com um prato (ou filme) e deixe na geladeira por 24 horas -
+            troque a água pelo menos uma vez durante este período.
           </li>
           <li>
-            Escorra a água e transfira a carne para a panela de pressão. Complete com água até a metade da panela, tampe e leve ao fogo médio. Assim que a panela começar a apitar, diminua o fogo e deixe cozinhar por 20 minutos. Desligue o fogo e espere toda a pressão sair antes de abrir (você pode colocar um garfo sob a válvula par acelerar o processo, mas isso encurta o tempo de vida da panela). 
+            Escorra a água e transfira a carne para a panela de pressão.
+            Complete com água até a metade da panela, tampe e leve ao fogo
+            médio. Assim que a panela começar a apitar, diminua o fogo e deixe
+            cozinhar por 20 minutos. Desligue o fogo e espere toda a pressão
+            sair antes de abrir (você pode colocar um garfo sob a válvula par
+            acelerar o processo, mas isso encurta o tempo de vida da panela).
           </li>
           <li>
-            Despreze a água do cozimento, junte o feijão-fradinho e repita o passo anterior, mas dessa vez deixe cozinhar por apenas 10 minutos. Atenção: o feijão não pode cozinhar demais e desmanchar. Após os 10 minutos de cozimento, desligue o fogo e, com um garfo, levante a válvula para tirar a pressão da panela. 
+            Despreze a água do cozimento, junte o feijão-fradinho e repita o
+            passo anterior, mas dessa vez deixe cozinhar por apenas 10 minutos.
+            Atenção: o feijão não pode cozinhar demais e desmanchar. Após os 10
+            minutos de cozimento, desligue o fogo e, com um garfo, levante a
+            válvula para tirar a pressão da panela.
           </li>
           <li>
-            Com uma pinça, transfira os cubos de carne para um prato e desfie com dois garfos - descarte os pedaços maiores de gordura. Sobre uma tigela, passe o feijão por uma peneira e reserve a água do cozimento. 
+            Com uma pinça, transfira os cubos de carne para um prato e desfie
+            com dois garfos - descarte os pedaços maiores de gordura. Sobre uma
+            tigela, passe o feijão por uma peneira e reserve a água do
+            cozimento.
           </li>
           <li>
-            Leve ao fogo médio uma panela média. Quando aquecer, regue com o azeite e acrescente a cebola. Tempere com o sal e refogue por cerca de 2 minutos, até murchar. Acrescente o arroz e mexa bem por cerca de 1 minuto para envolver todos os grãos com o azeite. 
+            Leve ao fogo médio uma panela média. Quando aquecer, regue com o
+            azeite e acrescente a cebola. Tempere com o sal e refogue por cerca
+            de 2 minutos, até murchar. Acrescente o arroz e mexa bem por cerca
+            de 1 minuto para envolver todos os grãos com o azeite.
           </li>
           <li>
-            Meça 2 xícaras (chá) da água do cozimento reservada e regue sobre o arroz. Junte a folha de louro, misture e deixe cozinhar em fogo médio. Assim que a água começar a secar e atingir o mesmo nível do arroz, diminua o fogo e tampe parcialmente a panela. Deixe cozinhar até o arroz absorver toda a água - para verificar, fure o arroz com um garfo e afaste alguns grãos do fundo da panela. 
+            Meça 2 xícaras (chá) da água do cozimento reservada e regue sobre o
+            arroz. Junte a folha de louro, misture e deixe cozinhar em fogo
+            médio. Assim que a água começar a secar e atingir o mesmo nível do
+            arroz, diminua o fogo e tampe parcialmente a panela. Deixe cozinhar
+            até o arroz absorver toda a água - para verificar, fure o arroz com
+            um garfo e afaste alguns grãos do fundo da panela.
           </li>
           <li>
-            Desligue o fogo e mantenha a panela tampada por 5 minutos para que os grãos terminem de cozinhar no próprio vapor. Enquanto isso, prepare o refogado.
+            Desligue o fogo e mantenha a panela tampada por 5 minutos para que
+            os grãos terminem de cozinhar no próprio vapor. Enquanto isso,
+            prepare o refogado.
           </li>
         </ol>
       </>
@@ -63,7 +92,10 @@ const links = [
     content: (
       <>
         <p>
-          Como cozinhar ovo? Os três passos estão bem descritos aqui. A diferença entre abrir o ovo e encontrar a gema do jeitinho que você queria (mole, cremosa ou dura) está em cronometrar o tempo de cozimento.
+          Como cozinhar ovo? Os três passos estão bem descritos aqui. A
+          diferença entre abrir o ovo e encontrar a gema do jeitinho que você
+          queria (mole, cremosa ou dura) está em cronometrar o tempo de
+          cozimento.
         </p>
         <H2>Ingredientes</H2>
         <ul>
@@ -72,13 +104,26 @@ const links = [
         <H2>Modo de Preparo</H2>
         <ol>
           <li>
-            Leve uma panela pequena com água ao fogo médio. Quando ferver, com cuidado e o  auxílio de uma colher, mergulhe o ovo e abaixe o fogo (se você colocar o ovo com cuidado, a casca não trinca).  Se preferir, com uma agulha faça um furinho na base do ovo e adicione caldo de limão à água (ele ajuda a manter a casca do ovo sem rachaduras). Mas basta colocar o ovo com delicadeza que a casca fica inteira.
+            Leve uma panela pequena com água ao fogo médio. Quando ferver, com
+            cuidado e o auxílio de uma colher, mergulhe o ovo e abaixe o fogo
+            (se você colocar o ovo com cuidado, a casca não trinca). Se
+            preferir, com uma agulha faça um furinho na base do ovo e adicione
+            caldo de limão à água (ele ajuda a manter a casca do ovo sem
+            rachaduras). Mas basta colocar o ovo com delicadeza que a casca fica
+            inteira.
           </li>
           <li>
-            Conte os minutos. Para gema cozida, mas sem passar do ponto, 12 minutos.  Para a gema cremosa, 7 minutos. Para o ovo mollet (clara cozida firme e gema mais líquida), 6 minutos. Para o ovo quente (clara cozida macia e gema bem mole), 4 minutos.
+            Conte os minutos. Para gema cozida, mas sem passar do ponto, 12
+            minutos. Para a gema cremosa, 7 minutos. Para o ovo mollet (clara
+            cozida firme e gema mais líquida), 6 minutos. Para o ovo quente
+            (clara cozida macia e gema bem mole), 4 minutos.
           </li>
           <li>
-            Com uma colher, tire o ovo da panela e mergulhe numa tigela com água fria filtrada até ele amornar. Para descascar, role delicadamente o ovo sobre a tábua e vá puxando os pedacinhos de casca. Passe novamente o ovo na tigela com água para descartar qualquer casquinha que tenha sobrado.
+            Com uma colher, tire o ovo da panela e mergulhe numa tigela com água
+            fria filtrada até ele amornar. Para descascar, role delicadamente o
+            ovo sobre a tábua e vá puxando os pedacinhos de casca. Passe
+            novamente o ovo na tigela com água para descartar qualquer casquinha
+            que tenha sobrado.
           </li>
         </ol>
       </>
@@ -90,11 +135,16 @@ const links = [
     content: (
       <>
         <p>
-          Bolo de cenoura fica macio, bem fofinho, no ponto para receber a calda de chocolate. No Panelinha, todas as receitas são testadas, preparadas pela equipe inúmeras vezes. Por isso sempre funciona.
+          Bolo de cenoura fica macio, bem fofinho, no ponto para receber a calda
+          de chocolate. No Panelinha, todas as receitas são testadas, preparadas
+          pela equipe inúmeras vezes. Por isso sempre funciona.
         </p>
         <H2>Ingredientes (bolo)</H2>
         <ul>
-          <li>3 cenouras médias (cerca de 360g ou 2 ¼ xícaras (chá) de cenoura descascada e ralada)</li>
+          <li>
+            3 cenouras médias (cerca de 360g ou 2 ¼ xícaras (chá) de cenoura
+            descascada e ralada)
+          </li>
           <li>4 ovos</li>
           <li>1 xícara (chá) de óleo de milho</li>
           <li>1 ½ xícara (chá) de açúcar</li>
@@ -106,22 +156,35 @@ const links = [
         <H2>Modo de Preparo (bolo)</H2>
         <ol>
           <li>
-            Preaqueça o forno a 180 ºC (temperatura média). Unte com manteiga uma fôrma retangular de 30 cm x 20 cm e 5 cm de altura. Polvilhe com farinha de trigo, chacoalhe e bata sobre a pia para tirar o excesso.
+            Preaqueça o forno a 180 ºC (temperatura média). Unte com manteiga
+            uma fôrma retangular de 30 cm x 20 cm e 5 cm de altura. Polvilhe com
+            farinha de trigo, chacoalhe e bata sobre a pia para tirar o excesso.
           </li>
           <li>
-            Numa tigela, coloque a farinha, o sal e o fermento, passando pela peneira. Misture e reserve. 
+            Numa tigela, coloque a farinha, o sal e o fermento, passando pela
+            peneira. Misture e reserve.
           </li>
           <li>
-            Lave e descasque as cenouras. Descarte a ponta da rama, corte as cenouras em rodelas e transfira para o liquidifcador. 
+            Lave e descasque as cenouras. Descarte a ponta da rama, corte as
+            cenouras em rodelas e transfira para o liquidifcador.
           </li>
           <li>
-            Junte o óleo às cenouras cortadas. Numa tigela pequena, quebre um ovo de cada vez e transfira para o liquidificador – se um estiver estragado, você não perde a receita. Acrescente o açúcar e bata bem até ficar liso, por cerca de 5 minutos. 
+            Junte o óleo às cenouras cortadas. Numa tigela pequena, quebre um
+            ovo de cada vez e transfira para o liquidificador – se um estiver
+            estragado, você não perde a receita. Acrescente o açúcar e bata bem
+            até ficar liso, por cerca de 5 minutos.
           </li>
           <li>
-            Transfira a mistura líquida para uma tigela grande e adicione aos poucos os ingredientes secos, misturando delicadamente com um batedor de arame para incorporar.
+            Transfira a mistura líquida para uma tigela grande e adicione aos
+            poucos os ingredientes secos, misturando delicadamente com um
+            batedor de arame para incorporar.
           </li>
           <li>
-            Com cuidado, transfira a massa para a fôrma e leve ao forno para assar por cerca de 45 minutos. Para saber se o bolo está pronto, espete um palito na massa: se sair limpo, pode tirar do forno; caso contrário, deixe por mais alguns minutos, até assar completamente. Deixe esfriar por 15 minutos antes de preparar a cobertura.
+            Com cuidado, transfira a massa para a fôrma e leve ao forno para
+            assar por cerca de 45 minutos. Para saber se o bolo está pronto,
+            espete um palito na massa: se sair limpo, pode tirar do forno; caso
+            contrário, deixe por mais alguns minutos, até assar completamente.
+            Deixe esfriar por 15 minutos antes de preparar a cobertura.
           </li>
         </ol>
         <H2>Ingredientes (cobertura)</H2>
@@ -134,23 +197,30 @@ const links = [
         <H2>Modo de Preparo (cobertura)</H2>
         <ol>
           <li>
-            Numa panela pequena junte o chocolate, o açúcar, a manteiga e a água. Leve ao fogo alto e mexa com o batedor de arame até ferver.
+            Numa panela pequena junte o chocolate, o açúcar, a manteiga e a
+            água. Leve ao fogo alto e mexa com o batedor de arame até ferver.
           </li>
           <li>
-            Depois que começar a ferver, mexa por mais 3 minutos, até a calda engrossar e desgrudar do fundo da panela. Regue a calda quente imediatamente sobre o bolo morno (ou frio) e espalhe com uma espátula para cobrir toda a superfície. Deixe a calda esfriar completamente e cristalizar antes de servir.
+            Depois que começar a ferver, mexa por mais 3 minutos, até a calda
+            engrossar e desgrudar do fundo da panela. Regue a calda quente
+            imediatamente sobre o bolo morno (ou frio) e espalhe com uma
+            espátula para cobrir toda a superfície. Deixe a calda esfriar
+            completamente e cristalizar antes de servir.
           </li>
         </ol>
       </>
-    )
-  }
-]
+    ),
+  },
+];
 
 export default function App() {
-  const [post, setPost] = useState(links[0]);
+  const [title, setTitle] = useState(links[0].title);
+  const [content, setContent] = useState(links[0].content);
 
   function handleChangePost(id) {
     const newPost = links.find((link) => link.id === id);
-    setPost(newPost);
+    setTitle(newPost.title);
+    setContent(newPost.content);
   }
 
   return (
@@ -158,8 +228,8 @@ export default function App() {
       <Header />
       <Menu />
       <Sidebar links={links} handleChangePost={handleChangePost} />
-      <ContentArea post={post} />
+      <ContentArea title={title} content={content} />
       <Footer />
     </div>
-  )
+  );
 }
